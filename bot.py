8fa71a36d1d81a9bf96c8c79f9ed7d9b108f31e8
assanalibatyrkhan <asankek12@gmail.com>
2024-06-10 10:51:11 +0500

    shows today's birthday guy

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import asyncio
 import filters
 import secret
 import messages
+import datetime
 
 DB_USER = secret.DB_USER
 DB_PASSWORD = secret.DB_PASSWORD
@@ -42,19 +43,36 @@ async def send_username(message: Message):
     await message.answer(f'@{message.from_user.username}')
 
 
-# @asanbl4
+# example@gmail.com
 @dp.message(filters.EmailFilter())
 async def show_student_info(message: Message):
     async with db_con_pool.acquire() as connection:
         email = message.text
         student = await connection.fetchrow("SELECT * FROM students WHERE email=$1", email)
         if student:
-            message_text = await messages.birthday_message(student['first_name'])
-            bd_image = FSInputFile('imgs/bd_image.jpg')
-            await bot.send_photo(message.chat.id, bd_image, caption=message_text)
-            await bot.send_message(message.chat.id, f"{student['first_name']} {student['last_name']}\n@{student['handle']}")
+            await send_bd_message(message, student)
         else:
             await message.answer(f'No students with handle: {email} found in database')
+
+
+@dp.message(Command('today'))
+async def show_today_birthday(message: Message):
+    async with db_con_pool.acquire() as connection:
+        students = await connection.fetch("SELECT * FROM students")
+        for student in students:
+            if student['birthday']:
+                if datetime.date.today().day == student['birthday'].day and datetime.date.today().month == student[
+                    'birthday'].month:
+                    await send_bd_message(message, student)
+        await bot.send_message(message.chat.id, "Request done")
+
+
+async def send_bd_message(message, student):
+    message_text = await messages.birthday_message(student['first_name'])
+    bd_image = FSInputFile('imgs/bd_image.jpg')
+    await bot.send_photo(message.chat.id, bd_image, caption=message_text)
+    await bot.send_message(message.chat.id,
+                           f"{student['first_name']} {student['last_name']}\n@{student['handle']}")
 
 
 async def main():
